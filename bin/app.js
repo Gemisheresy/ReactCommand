26cd7f9869d1f4e5bd64cff5ae1bbc11815b0e85
Gemisheresy <Gemisheresy@gmail.com>
2017-10-20 14:45:11 -0400

    First Commit

---
+++ bin/app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const util =require('util')
+const  exec  = util.promisify(require('child_process').exec)
+const rename = util.promisify(require('fs').rename);
+
+const program = require('commander');
+const ErrorHandle = (error) => console.log(error);
+async function gitAndRename (string){
+   try{
+        await exec(
+            'git clone https://github.com/Gemisheresy/ReactBolier.git'
+            )
+        await rename(
+        './ReactBolier',
+        `./${string}`
+    )
+   }
+   catch (error){
+        ErrorHandle(error)
+   }
+}
+function echoFunction(string){
+    console.log(string)
+
+}
+program
+    .version('0.1.0')
+    .command("init <string>")
+    .action(gitAndRename);
+
+
+program.parse(process.argv);
